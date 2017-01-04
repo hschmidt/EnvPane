hschmidt/EnvPane

commit 3c4f0fc1cd6851e67a21bfd69611a6c8fbe86dd7

On 10.10 and newer use XPC-based API to launchd (fixes #11)

EnvLib/launchd_legacy.c
@@ -1,0 +1,68 @@
+/*
+ * Copyright 2012, 2017 Hannes Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "launchd_legacy.h"
+
+#include <errno.h>
+#include <stdio.h>
+
+#include "launch_priv.h"
+#include "log.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+
+void envlib_setenv( const char *key, const char *value )
+{
+    launch_data_t request, entry, valueData, response;
+
+    request = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
+    entry = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
+    valueData = launch_data_new_string( value );
+    launch_data_dict_insert( entry, valueData, key );
+    launch_data_dict_insert( request, entry, LAUNCH_KEY_SETUSERENVIRONMENT );
+
+    response = launch_msg( request );
+    launch_data_free( request );
+
+    if( response ) {
+        launch_data_free( response );
+    } else {
+        NSLog( CFSTR( "launch_msg( '%s' ): %s" ), LAUNCH_KEY_SETUSERENVIRONMENT, strerror( errno ) );
+    }
+}
+
+void envlib_unsetenv( const char *key )
+{
+    launch_data_t request, keys, keyData, response;
+
+    request = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
+    keyData = launch_data_new_string( key );
+    keys = launch_data_alloc( LAUNCH_DATA_ARRAY );
+    launch_data_array_set_index(keys, keyData, 0);
+    launch_data_dict_insert( request, keys, LAUNCH_KEY_UNSETUSERENVIRONMENT );
+
+    response = launch_msg( request );
+    launch_data_free( request );
+    if( response ) {
+        launch_data_free( response );
+    } else {
+        NSLog( CFSTR( "launch_msg( '%s' ): %s" ), LAUNCH_KEY_UNSETUSERENVIRONMENT, strerror( errno ) );
+    }
+}
+
+#pragma GCC diagnostic pop

EnvLib/Environment.m
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Hannes Schmidt
+ * Copyright 2012, 2017 Hannes Schmidt
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,9 +21,8 @@
 
 #include "Constants.h"
 
-#include <errno.h>
-
-#include "launch_priv.h"
+#include "launchd_xpc.h"
+#include "launchd_legacy.h"
 
 @implementation Environment
 
@@ -87,48 +86,7 @@ static NSString* savedEnvironmentPath;
     return [env initWithDictionary: dict];
 }
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-    void envlib_setenv( const char *key, const char *value )
-    {
-        launch_data_t request, entry, valueData, response;
-
-        request = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
-        entry = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
-        valueData = launch_data_new_string( value );
-        launch_data_dict_insert( entry, valueData, key );
-        launch_data_dict_insert( request, entry, LAUNCH_KEY_SETUSERENVIRONMENT );
-
-        response = launch_msg( request );
-        launch_data_free( request );
-
-        if( response ) {
-            launch_data_free( response );
-        } else {
-            NSLog( @"launch_msg( \"%s\" ): %s", LAUNCH_KEY_SETUSERENVIRONMENT, strerror( errno ) );
-        }
-    }
-
-    void envlib_unsetenv( const char *key )
-    {
-        launch_data_t request, keyData, response;
-
-        request = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
-        keyData = launch_data_new_string( key );
-        launch_data_dict_insert( request, keyData, LAUNCH_KEY_UNSETUSERENVIRONMENT );
-
-        response = launch_msg( request );
-        launch_data_free( request );
-
-        if( response ) {
-            launch_data_free( response );
-        } else {
-            NSLog( @"launch_msg( \"%s\" ): %s", LAUNCH_KEY_UNSETUSERENVIRONMENT, strerror( errno ) );
-        }
-    }
-
-#pragma GCC diagnostic pop
 
 - (void) export
 {
@@ -157,28 +115,48 @@ static NSString* savedEnvironmentPath;
      * Iterate over each variable in the new environment and ...
      */
     [_dict enumerateKeysAndObjectsUsingBlock: ^ ( NSString *key, NSString *value, BOOL *stop ) {
-         if( value != nil ) {
-             /*
-              * ... export the variable, ...
-              */
-             NSLog( @"Setting '%@' to '%@'", key, value );
-             envlib_setenv( key.UTF8String, value.UTF8String );
-             /*
-              * ... remove it from the variables to be deleted, ...
-              */
-             [deletedVariables removeObject: key];
-             /*
-              * ... and add it to the list of variables for book-keeping.
-              */
-             [variables addObject: key];
+        if( value != nil ) {
+            /*
+             * ... export the variable, ...
+             */
+            if( kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber10_10 ) {
+                NSLog( @"Setting '%@' to '%@' using legacy launchd API.", key, value );
+                envlib_setenv( key.UTF8String, value.UTF8String );
+            } else {
+                NSLog( @"Setting '%@' to '%@' using XPC launchd API.", key, value );
+                EnvEntry env[2];
+                env[0].name = key.UTF8String;
+                env[0].value = value.UTF8String;
+                env[1].name = NULL;
+                env[1].value = NULL;
+                envlib_setenv_xpc(env);
+            }
+            /*
+             * ... remove it from the variables to be deleted, ...
+             */
+            [deletedVariables removeObject: key];
+            /*
+             * ... and add it to the list of variables for book-keeping.
+             */
+            [variables addObject: key];
          }
      }];
     /*
      * Delete all variables that were set previously but have been removed.
      */
     [deletedVariables enumerateObjectsUsingBlock: ^ ( NSString* key, BOOL *stop ) {
-         NSLog( @"Unsetting '%@'", key );
-         envlib_unsetenv( key.UTF8String );
+        if( kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber10_10 ) {
+            NSLog( @"Unsetting '%@' using legacy launchd API.", key );
+            envlib_unsetenv( key.UTF8String );
+        } else {
+            NSLog( @"Unsetting '%@' using XPC launchd API.", key );
+            EnvEntry env[2];
+            env[0].name = key.UTF8String;
+            env[0].value = NULL;
+            env[1].name = NULL;
+            env[1].value = NULL;
+            envlib_setenv_xpc(env);
+        }
      }];
     /*
      * Export the list of variables such that the next invocation can determine

EnvLib/launchd_legacy.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2012, 2017 Hannes Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef launchd_legacy_h
+#define launchd_legacy_h
+
+void envlib_setenv( const char *key, const char *value );
+void envlib_unsetenv( const char *key );
+
+#endif /* launchd_legacy_h */

EnvLib/launchd_xpc.c
@@ -1,0 +1,78 @@
+/*
+ * Copyright 2012, 2017 Hannes Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "launchd_xpc.h"
+#include <xpc/xpc.h>
+#include "log.h"
+
+struct xpc_global_data {
+    uint64_t	a;
+    uint64_t	xpc_flags;
+    mach_port_t	task_bootstrap_port;
+#if __LP64__
+    uint32_t	padding;
+#endif
+    xpc_object_t	xpc_bootstrap_pipe;
+};
+
+struct _os_alloc_once_s {
+    long once;
+    void *ptr;
+};
+
+extern struct _os_alloc_once_s _os_alloc_once_table[];
+
+extern int xpc_pipe_routine(xpc_object_t *pipe, xpc_object_t *request, xpc_object_t **response);
+extern int xpc_dictionary_set_mach_send(xpc_object_t xdict, const char *key, mach_port_t);
+extern char *xpc_strerror(int64_t);
+
+
+bool envlib_setenv_xpc( EnvEntry env[] ) {
+    struct xpc_global_data *xpc_gd = (struct xpc_global_data *) _os_alloc_once_table[1].ptr;
+
+    xpc_object_t envvars = xpc_dictionary_create( NULL, NULL, 0 );
+    for( EnvEntry* entry = env; entry->name; entry++ ) {
+        if( entry->value ) {
+            xpc_dictionary_set_string( envvars, entry->name, entry->value);
+        } else {
+            xpc_dictionary_set_value( envvars, entry->name, xpc_null_create());
+        }
+    }
+    xpc_object_t dict = xpc_dictionary_create( NULL, NULL, 0 );
+    xpc_dictionary_set_uint64( dict, "subsystem", 3 );
+    xpc_dictionary_set_uint64( dict, "routine", 0x333 );
+    xpc_dictionary_set_value( dict, "envvars", envvars );
+    xpc_dictionary_set_uint64( dict, "type", 7 );
+    xpc_dictionary_set_uint64( dict, "handle", 0 );
+    xpc_dictionary_set_bool( dict, "legacy", 1 );
+    xpc_dictionary_set_bool( dict, "legacy", 1 );
+    xpc_dictionary_set_mach_send( dict, "domain-port", xpc_gd[1].task_bootstrap_port );
+
+    xpc_object_t *outDict = NULL;
+
+    int rc = xpc_pipe_routine( xpc_gd->xpc_bootstrap_pipe, dict, &outDict );
+    if( rc == 0 && outDict ) {
+        int64_t err = xpc_dictionary_get_int64( outDict, "error" );
+        if( err ) {
+            NSLog( CFSTR( "Error:  %lld - %s\n" ), err, xpc_strerror(err) );
+        } else {
+            return true;
+        }
+    } else {
+        NSLog( CFSTR( "Error: xpc_pipe_routine() failed\n" ) );
+    }
+    return false;
+}

EnvLib/launchd_xpc.h
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2012, 2017 Hannes Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef launchd_xpc_h
+#define launchd_xpc_h
+
+#include "stdbool.h"
+
+typedef struct {
+    const char *name;
+    const char *value;
+} EnvEntry;
+
+
+bool envlib_setenv_xpc( EnvEntry env[] );
+
+#endif /* launchd_xpc_h */

EnvLib/log.h
@@ -1,0 +1,26 @@
+/*
+ * Copyright 2012, 2016 Hannes Schmidt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef log_h
+#define log_h
+
+// http://stackoverflow.com/questions/3060121/core-foundation-equivalent-for-nslog
+
+#include <CoreFoundation/CFString.h>
+
+void NSLog(CFStringRef format, ...);
+
+#endif /* log_h */

EnvPane.xcodeproj/project.pbxproj
@@ -22,6 +22,11 @@
 		D154535216CC4045008A3ADB /* NSFileManager+EnvLib.m in Sources */ = {isa = PBXBuildFile; fileRef = D154535016CC4045008A3ADB /* NSFileManager+EnvLib.m */; };
 		D154535516CC74AB008A3ADB /* NSDictionary+EnvLib.h in Headers */ = {isa = PBXBuildFile; fileRef = D154535316CC74AB008A3ADB /* NSDictionary+EnvLib.h */; };
 		D154535616CC74AB008A3ADB /* NSDictionary+EnvLib.m in Sources */ = {isa = PBXBuildFile; fileRef = D154535416CC74AB008A3ADB /* NSDictionary+EnvLib.m */; };
+		D167214C1E1B039900743608 /* launchd_xpc.c in Sources */ = {isa = PBXBuildFile; fileRef = D167214A1E1B039900743608 /* launchd_xpc.c */; };
+		D167214D1E1B039900743608 /* launchd_xpc.h in Headers */ = {isa = PBXBuildFile; fileRef = D167214B1E1B039900743608 /* launchd_xpc.h */; };
+		D16721501E1B148400743608 /* launchd_legacy.c in Sources */ = {isa = PBXBuildFile; fileRef = D167214E1E1B148400743608 /* launchd_legacy.c */; };
+		D16721511E1B148400743608 /* launchd_legacy.h in Headers */ = {isa = PBXBuildFile; fileRef = D167214F1E1B148400743608 /* launchd_legacy.h */; };
+		D16721551E1B2ECA00743608 /* log.h in Headers */ = {isa = PBXBuildFile; fileRef = D16721531E1B2ECA00743608 /* log.h */; };
 		D18C420A16CC1DC700EF620B /* Error.h in Headers */ = {isa = PBXBuildFile; fileRef = D18C420816CC1DC700EF620B /* Error.h */; };
 		D18C420C16CC1E0C00EF620B /* Error.m in Sources */ = {isa = PBXBuildFile; fileRef = D18C420B16CC1E0C00EF620B /* Error.m */; };
 		D197F2191683EF98001CD067 /* net.hannesschmidt.EnvAgent.plist in CopyFiles */ = {isa = PBXBuildFile; fileRef = E2ADE0C2083951BC000B79B2 /* net.hannesschmidt.EnvAgent.plist */; };
@@ -104,6 +109,11 @@
 		D154535016CC4045008A3ADB /* NSFileManager+EnvLib.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = "NSFileManager+EnvLib.m"; sourceTree = "<group>"; };
 		D154535316CC74AB008A3ADB /* NSDictionary+EnvLib.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = "NSDictionary+EnvLib.h"; sourceTree = "<group>"; };
 		D154535416CC74AB008A3ADB /* NSDictionary+EnvLib.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = "NSDictionary+EnvLib.m"; sourceTree = "<group>"; };
+		D167214A1E1B039900743608 /* launchd_xpc.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; path = launchd_xpc.c; sourceTree = "<group>"; };
+		D167214B1E1B039900743608 /* launchd_xpc.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = launchd_xpc.h; sourceTree = "<group>"; };
+		D167214E1E1B148400743608 /* launchd_legacy.c */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.c; path = launchd_legacy.c; sourceTree = "<group>"; };
+		D167214F1E1B148400743608 /* launchd_legacy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = launchd_legacy.h; sourceTree = "<group>"; };
+		D16721531E1B2ECA00743608 /* log.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = log.h; sourceTree = "<group>"; };
 		D18C420816CC1DC700EF620B /* Error.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = Error.h; sourceTree = "<group>"; };
 		D18C420B16CC1E0C00EF620B /* Error.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = Error.m; sourceTree = "<group>"; };
 		D19BDB5C1671286100C6467B /* EnvPane.prefPane */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = EnvPane.prefPane; sourceTree = BUILT_PRODUCTS_DIR; };
@@ -169,6 +179,7 @@
 		D19BDB5D1671286100C6467B /* Frameworks */ = {
 			isa = PBXGroup;
 			children = (
+				D1EFCA5016912C6600013C1A /* WebKit.framework */,
 				D10BFDE21688E81600C6A4CF /* ServiceManagement.framework */,
 				D10BFDDB1688DE0B00C6A4CF /* SecurityFoundation.framework */,
 				D1E16E66167292D100724499 /* CoreFoundation.framework */,
@@ -239,6 +250,11 @@
 				D154535016CC4045008A3ADB /* NSFileManager+EnvLib.m */,
 				D154535316CC74AB008A3ADB /* NSDictionary+EnvLib.h */,
 				D154535416CC74AB008A3ADB /* NSDictionary+EnvLib.m */,
+				D167214A1E1B039900743608 /* launchd_xpc.c */,
+				D167214B1E1B039900743608 /* launchd_xpc.h */,
+				D167214E1E1B148400743608 /* launchd_legacy.c */,
+				D167214F1E1B148400743608 /* launchd_legacy.h */,
+				D16721531E1B2ECA00743608 /* log.h */,
 			);
 			path = EnvLib;
 			sourceTree = "<group>";
@@ -246,7 +262,6 @@
 		E2ADE09E08395023000B79B2 = {
 			isa = PBXGroup;
 			children = (
-				D1EFCA5016912C6600013C1A /* WebKit.framework */,
 				D1FC1DBC168F90AB004F5901 /* README.md */,
 				D1F739AE168F7F40009F8342 /* NOTICE */,
 				D1F739AD168EFA01009F8342 /* LICENSE */,
@@ -287,9 +302,12 @@
 			files = (
 				D1334E3A16858FFC0018F00D /* Environment.h in Headers */,
 				D1F739A2168D3E6B009F8342 /* Constants.h in Headers */,
+				D167214D1E1B039900743608 /* launchd_xpc.h in Headers */,
 				D18C420A16CC1DC700EF620B /* Error.h in Headers */,
 				D154535116CC4045008A3ADB /* NSFileManager+EnvLib.h in Headers */,
+				D16721551E1B2ECA00743608 /* log.h in Headers */,
 				D154535516CC74AB008A3ADB /* NSDictionary+EnvLib.h in Headers */,
+				D16721511E1B148400743608 /* launchd_legacy.h in Headers */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -422,6 +440,8 @@
 			buildActionMask = 2147483647;
 			files = (
 				D1334E3B16858FFC0018F00D /* Environment.m in Sources */,
+				D167214C1E1B039900743608 /* launchd_xpc.c in Sources */,
+				D16721501E1B148400743608 /* launchd_legacy.c in Sources */,
 				D18C420C16CC1E0C00EF620B /* Error.m in Sources */,
 				D154535216CC4045008A3ADB /* NSFileManager+EnvLib.m in Sources */,
 				D154535616CC74AB008A3ADB /* NSDictionary+EnvLib.m in Sources */,

commit 411da8dedfe0301142c1bad3ed475a018a2fcd3e

Fix: Can’t unset variables with legacy API (connected to #11)
Regression introduced in 3c4f0fc1cd6851e67a21bfd69611a6c8fbe86dd7.

EnvLib/launchd_legacy.c
@@ -48,13 +48,11 @@ void envlib_setenv( const char *key, const char *value )
 
 void envlib_unsetenv( const char *key )
 {
-    launch_data_t request, keys, keyData, response;
+    launch_data_t request, keyData, response;
 
     request = launch_data_alloc( LAUNCH_DATA_DICTIONARY );
     keyData = launch_data_new_string( key );
-    keys = launch_data_alloc( LAUNCH_DATA_ARRAY );
-    launch_data_array_set_index(keys, keyData, 0);
-    launch_data_dict_insert( request, keys, LAUNCH_KEY_UNSETUSERENVIRONMENT );
+    launch_data_dict_insert( request, keyData, LAUNCH_KEY_UNSETUSERENVIRONMENT );
 
     response = launch_msg( request );
     launch_data_free( request );